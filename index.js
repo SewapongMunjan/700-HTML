aae68cd18c97062ab0565d8601609d3aa345cfaa
SewapongMunjan <151804902+SewapongMunjan@users.noreply.github.com>
2024-02-29 12:27:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+// 1. GET /users สำหรับ get users ทั้งหมดที่บันทึกเข้าไปออกมา
+// 2. POST /users สำหรับการสร้าง users ใหม่บันทึกเข้าไป
+// 3. GET /users/:id สำหรับการดึง users รายคนออกมา
+// 4. PUT /users/:id สำหรับการแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+// 5. DELETE /users/:id สำหรับการลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+
+let users = [];
+let counter = 1;
+app.get("/", (req, res) => {
+  let user = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 25,
+  }
+
+  res.json(user);
+});
+
+
+// 1. GET /users สำหรับ get users ทั้งหมดที่บันทึกเข้าไปออกมา
+app.get("/users", (req, res) => {
+    const filterUsers = users.map(user => {
+        return {
+            id: user.id,
+            firstname: user.firstname,
+            lastname: user.lastname, 
+            fullname: user.firstname + ' ' + user.lastname
+        }
+    })
+  res.json(filterUsers);
+});
+
+
+
+// 2. POST /users สำหรับการสร้าง users ใหม่บันทึกเข้าไป
+app.post("/users", (req, res) => {
+  let user = req.body;
+  user.id = counter;
+  counter++;
+  users.push(user);
+  res.json({
+    message: "Add new user successfully",
+    user: user
+  });
+  res.send(req.body);
+});
+
+
+
+// 3. GET /users/:id สำหรับการดึง users รายคนออกมา
+app.get("/users/:id", (req, res) => {
+    let id = req.params.id;
+    //find index
+    let selectIndex = users.findIndex(user => user.id == id)
+  res.json(users[selectIndex]);
+});
+
+
+
+// 4. PUT /user/:id สำหรับการแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.put("/user/:id", (req, res) => {
+  let id = req.params.id;
+  let updateUser = req.body;
+  // 1. หา users จาก id ที่ส่งมา
+  let selectIndex = users.findIndex(user => user.id == id);
+
+  // Update ข้อมูล user
+    users[selectIndex].firstname = updateUser.firstname || users[selectIndex].firstname;
+    users[selectIndex].lastname = updateUser.lastname || users[selectIndex].lastname;
+    users[selectIndex].age = updateUser.age || users[selectIndex].age;
+    users[selectIndex].gender = updateUser.gender || users[selectIndex].gender;
+    
+    // 2. Update users นั้น
+  // users[selectIndex].firstname = updateUser.firstname || users[selectIndex].firstname;
+  // users[selectIndex].lastname = updateUser.lastname || users[selectIndex].lastname;
+  res.json({
+    message: 'Update user succussfully',
+    data: {
+      user: updateUser,
+      indexUpdate: selectIndex
+    }
+    // 3. users ที่ Update ใหม่ ทำการ Update กลับเข้าไปที่ users ตัวเดิม
+  });
+
+});
+
+
+
+// 5. DELETE /users/:id สำหรับการลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.delete("/users/:id", (req, res) => {
+  let id = req.params.id;
+  // 1. หาข้อมูล User จาก index
+  let selectIndex = users.findIndex(user => user.id == id);
+  // 2. ลบข้อมูล User
+  // delete users[selectIndex]; ลบแบบนี้จะมีค่า Null ทิ้งไว้
+  users.splice(selectIndex,1); // ลบแบบนี้จะไม่ทิ้งค่า Null ไว้ (ดีกว่า)
+  res.json({
+    message: 'Delete user succussfully',
+    data: {
+      indexUpdate: selectIndex
+    }
+  });
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port ${port}.`);
+});
